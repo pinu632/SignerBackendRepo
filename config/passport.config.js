8550a6e1d1f89a4467e713e7868b275b018d2244
pinu632 <pinu632@gmail.com>
2025-06-10 20:44:42 +0530

    minor changes

--- config/passport.config.js
+++ config/passport.config.js
@@ -16,7 +16,7 @@ dotenv.config()
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'https://signerbackendrepo.onrender.com//api/auth/google/callback',
+    callbackURL: 'https://signerbackendrepo.onrender.com/api/auth/google/callback',
     scope: ['profile', 'email'] 
   },
   async function (accessToken, refreshToken, profile, done) {
